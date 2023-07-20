19001d9be622c27b83c83335278b7a0b53bfa65c
TanayDeshmukh <43087469+tanay0801@users.noreply.github.com>
2023-07-20 16:57:16 +0530

    Add files via upload

---
+++ Algorithm.java
@@ -0,0 +1,14 @@
+class Algorithm 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(1234);
+		System.out.println(20.0);
+		System.out.println('a');
+		System.out.println('%');
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println("abc");
+		System.out.println(args = null);
+	}
+}
