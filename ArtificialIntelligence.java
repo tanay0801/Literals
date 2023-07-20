fe0f67ba38d5cbdea6669d3cb9d8695d38e79f19
TanayDeshmukh <43087469+tanay0801@users.noreply.github.com>
2023-07-20 16:58:04 +0530

    Add files via upload

---
+++ ArtificialIntelligence.java
@@ -0,0 +1,11 @@
+class ArtificialIntelligence 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(100 + 100);
+		System.out.println(200 - 100);
+		System.out.println(10 * 100);
+		System.out.println(10 % 2);
+		System.out.println(10 / 2);
+	}
+}
